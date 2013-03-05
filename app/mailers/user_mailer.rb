2f204861c74d62ecbcad67a5d7fa364ac6597598
Scott Wolff <scott.wolff@tuscan.ca>
2013-03-04 21:54:55 -0700

    Replace testing URL with production URL in password reset mailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@info.ca"
+  default from: "info@flightclimbers.com"
 
   def welcome_email(user)
   	@user = user
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
 
 def reset_password_email(user)
   @user = user
-  @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
+  @url  = "http://flightclimbers.com/password_resets/#{user.reset_password_token}/edit"
   mail(:to => user.email,
        :subject => "Your password has been reset")
 end
